8e2dd759a62732ce796ca7d2649ca283f8351e11
marsacolombo <timbiaou@gmail.com>
2024-07-05 14:26:38 +0200

    cleaning

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // @ts-ignore
   test: {
     include:['src/**/*.test.ts']
   }
